1f7efccd719f6b1b7cc14172828402f71028294e
Guilherme Bastos <gbastos127@gmail.com>
2020-10-21 21:46:48 -0300

    Tooltip refatorarda

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './modules/scroll-suave';
 import Accordion from './modules/accordion';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
@@ -24,7 +24,9 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
-initTooltip();
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
