edabb79e19dcfc533598e6230311922940d01a52
Guilherme Bastos <gbastos127@gmail.com>
2020-10-21 21:03:58 -0300

    modal refatorado com class

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from './modules/scroll-suave';
 import Accordion from './modules/accordion';
 import TabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
+import Modal from './modules/modal';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
@@ -21,8 +21,9 @@ accordion.init();
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
 
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
